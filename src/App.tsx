9a3b07e9fd4dd5a69e761b5b7190c5717d882444
Tushar Garg <itushargarg@gmail.com>
2019-12-18 23:26:15 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import CReducer from "./reducers";
+import "./App.css";
+import Index from "./pages";
+import { composeWithDevTools } from "redux-devtools-extension";
+import STMT from "./pages/stmt";
+import menu from "./pages/menu";
+import Bank from "./pages/bank";
 
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  CReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+/* eslint-enable */
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Route exact path="/" component={Index} />
+        <Route path="/menu" component={menu} />
+        <Route path="/stmt/:id" component={STMT} />
+        <Route path="/banks" component={Bank} />
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
